3a13fa1ade84ba5d28228b7a305e58ec6a686812
kao75 <kao75@pitt.edu>
2020-06-17 17:56:19 -0400

    added button for settings page

--- app/src/main/java/com/example/rpag/MainActivity.java
+++ app/src/main/java/com/example/rpag/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.rpag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureSettingsButton();
+    }
+
+    private void configureSettingsButton()
+    {
+        Button settingsButton = (Button) findViewById(R.id.settingsWrenchButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
     }
 }
