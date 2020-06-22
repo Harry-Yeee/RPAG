efe6483baee518eb75a63ceebedf2dde5d629e73
HarryYeee <Harry.yeee@gamil.com>
2020-06-22 15:48:29 -0400

    Budget UI First Commit

--- app/src/main/java/com/example/rpag/MainActivity.java
+++ app/src/main/java/com/example/rpag/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.rpag;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openBudget = (Button)findViewById(R.id.budgetInterface_btn);
+
+        openBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, budgetUI.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/rpag/budgetUI.java
@@ -0,0 +1,14 @@
+package com.example.rpag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class budgetUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_ui);
+    }
+}
