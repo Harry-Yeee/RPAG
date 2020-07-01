3026269feae51d626f5e535427816235cad6f119
HanlanYang <mr.jackie.y@icloud.com>
2020-07-01 15:55:03 -0400

    #3: Make back button work

--- app/src/main/java/com/example/rpag/MainActivity.java
+++ app/src/main/java/com/example/rpag/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -40,5 +41,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, manageActivity.class));
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent,0);
+        return true;
     }
 }
--- app/src/main/java/com/example/rpag/manageActivity.java
+++ app/src/main/java/com/example/rpag/manageActivity.java
@@ -1,9 +1,9 @@
 package com.example.rpag;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 
 public class manageActivity extends AppCompatActivity {
@@ -14,27 +14,9 @@ public class manageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manage);
 
         // Test
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
-
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.settings_activity);
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .replace(R.id.settings, new SettingsActivity.SettingsFragment())
-//                .commit();
-//        ActionBar actionBar = getSupportActionBar();
-//        if (actionBar != null) {
-//            actionBar.setDisplayHomeAsUpEnabled(true);
-//        }
-//    }
-//
-//    public static class SettingsFragment extends PreferenceFragmentCompat {
-//        @Override
-//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-//            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-//        }
-//    }
 }
